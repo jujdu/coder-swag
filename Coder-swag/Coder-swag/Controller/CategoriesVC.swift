6937bb7e3554f93178543d0ae40e12561a6eb959
jujdu <michael.sidoruk@icloud.com>
2019-07-21 02:06:03 +0300

    lesson-02,-03,-04 are done

--- Coder-swag/Coder-swag/Controller/CategoriesVC.swift
+++ Coder-swag/Coder-swag/Controller/CategoriesVC.swift
@@ -14,6 +14,7 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         catergoryTable.dataSource = self
         catergoryTable.delegate = self
     }
@@ -33,5 +34,22 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        
+        }
+    }
 }
 
